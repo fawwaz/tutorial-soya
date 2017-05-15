3b2eb48d228d4b976f8cc911da35d30450f60201
fawwaz muhammad <fawwaz.muhammad@traveloka.com>
2017-05-15 12:41:17 +0700

    finished step 3.3-3.4 : enabling user interaction

--- src/pages/IndexPageContent.js
+++ src/pages/IndexPageContent.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import Form from 'soya/lib/data/redux/form/Form';
 import Header from '../components/Header/Header';
 import InputText from '../components/InputText/InputText';
 import TodoWrapper from '../components/TodoWrapper/TodoWrapper';
 import Footer from '../components/Footer/Footer';
 import FilterWrapper from '../components/FilterWrapper/FilterWrapper';
+import TodoSegment from '../segments/TodoSegment';
+import FormSegment from 'soya/lib/data/redux/form/FormSegment';
+import connect from 'soya/lib/data/redux/connect';
+
+const FORM_ID = 'todo_form';
 
 class IndexPageContent extends React.Component {
 
+  static getSegmentDependencies() {
+    return [TodoSegment];
+  }
+
+
+  constructor(props){
+    super(props);
+    if(props.context) {
+      this.form = new Form(this.props.context.store, FORM_ID);
+    }
+  }
+
+  listenEnter(event) {
+    const form = this.form;
+    const todostore = this.context.store;
+    if (event.key === 'Enter') {
+      const ACTION_CREATOR = TodoSegment.getActionCreator();
+
+      const promise = todostore.query(FormSegment.id(), {
+        formId: FORM_ID,
+        type: 'field',
+        fieldName: 'input_todo'
+      }).then((retval) => {
+        todostore
+          .dispatch(ACTION_CREATOR.add(retval.value))
+          .then(()=>{
+            form.setValue('input_todo','');
+          });
+      });
+    }
+  }
 
   render() {
     require('../../assets/base.css');
@@ -17,8 +54,12 @@ class IndexPageContent extends React.Component {
         <section className="todoapp">
           <Header text="Soya Todo">
             <InputText
-              type={InputText.TYPE.NEW}
+              form={this.form}
+              name="input_todo"
+              context={this.props.context}
+              type='new-todo'
               placeholder="What need to be done ?"
+              onKeyPressed={this.listenEnter}
             />
           </Header>
           <section className="main">
@@ -50,6 +91,6 @@ class IndexPageContent extends React.Component {
   }
 }
 
-export default IndexPageContent;
+export default connect(IndexPageContent);
 
 
